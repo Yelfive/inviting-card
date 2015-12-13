3222c769a29e3d675e0ff55aaafc3c95ee8807d2
Felix <1449704765@qq.com>
2015-12-13 22:52:10 +0800

    trail 3

--- js/base.js
+++ js/base.js
@@ -3,7 +3,11 @@
  */
 
 
-setTimeout(function () {
+document.onclick = function () {
+    $('#loading').css({opacity: 0})
+        .children('img')
+        .css({left: 100, top: 300, width: 500, height: 324});
+    return;
     var loading = document.querySelector('#loading');
     var style = loading.style;
     loading.style.opacity = 0;
@@ -11,4 +15,4 @@ setTimeout(function () {
     loading.style.height = '150%';
     style.left = '-100px';
     style.top = '-100px';
-},1000);
+};
