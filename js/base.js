27271418f692624914368d3757770f34e5738024
Felix <1449704765@qq.com>
2015-12-14 22:19:36 +0800

    trial 4

--- js/base.js
+++ js/base.js
@@ -2,17 +2,29 @@
  * Created by Administrator on 2015/12/12.
  */
 
-
-document.onclick = function () {
-    $('#loading').css({opacity: 0})
-        .children('img')
-        .css({left: 100, top: 300, width: 500, height: 324});
-    return;
-    var loading = document.querySelector('#loading');
-    var style = loading.style;
-    loading.style.opacity = 0;
-    loading.style.width = '150%';
-    loading.style.height = '150%';
-    style.left = '-100px';
-    style.top = '-100px';
+var loadingImg = [500, 324];
+window.getOffsetTop = function (h) {
+    h = !h ? document.getElementById('loading').getElementsByTagName('img').item(0).clientHeight : h;
+    var t = (window.innerHeight - h) / 2;
+    return t;
 };
++(function () {
+    $('#loading img').css({top: window.getOffsetTop()});
+
+    $('#menu').click(function () {
+        alert('Clicked!');
+    });
+}());
+
++(function () {
+    document.onclick = function () {
+        var h = window.innerWidth / loadingImg[0] * loadingImg[1];
+        $('#loading').css({opacity: 0})
+            .children('img')
+            .css({height: h, 'margin-bottom': getOffsetTop(h)});
+            //.css({height: h, top: getOffsetTop(h)});
+        setTimeout(function () {
+            $('#loading').remove();
+        }, 500);
+    };
+}());
