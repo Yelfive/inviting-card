4bb35cc296643b4e69dac514898a214faebfd78c
Felix <1449704765@qq.com>
2015-12-15 22:45:24 +0800

    webhook test

--- js/base.js
+++ js/base.js
@@ -1,30 +1,61 @@
 /**
  * Created by Administrator on 2015/12/12.
  */
+//
+//var loadingImg = [500, 324];
+//window.getOffsetTop = function (h) {
+//    h = !h ? document.getElementById('loading').getElementsByTagName('img').item(0).clientHeight : h;
+//    var t = (window.innerHeight - h) / 2;
+//    return t;
+//};
+//+(function () {
+//    $('#loading img').css({top: window.getOffsetTop()});
+//
+//    $('#menu').click(function () {
+//        alert('Clicked!');
+//    });
+//}());
+//
+//+(function () {
+//    document.onclick = function () {
+//        var h = window.innerWidth / loadingImg[0] * loadingImg[1];
+//        $('#loading').css({opacity: 0})
+//            .children('img')
+//            .css({height: h, 'top': getOffsetTop(h)});
+//        setTimeout(function () {
+//            $('#loading').remove();
+//        }, 500);
+//    };
+//}());
 
-var loadingImg = [500, 324];
-window.getOffsetTop = function (h) {
-    h = !h ? document.getElementById('loading').getElementsByTagName('img').item(0).clientHeight : h;
-    var t = (window.innerHeight - h) / 2;
-    return t;
-};
-+(function () {
-    $('#loading img').css({top: window.getOffsetTop()});
+require.config({
+    packages: [
+        {
+            name: 'zrender',
+            location: '../js/zrender',
+            main: 'zrender'
+        }
+    ]
+});
 
-    $('#menu').click(function () {
-        alert('Clicked!');
+require(['zrender', 'zrender/shape/Image'], function (zrender, ImageShape) {
+    var zr = zrender.init(document.body);
+    var image = new ImageShape({
+        position: [0, 0],
+        scale: [1, 1],
+        style: {
+            x: 0,
+            y: 0,
+            image: '../images/flying_elephant.jpg'
+        }
     });
-}());
+    zr.addElement(image);
+    zr.render();
 
-+(function () {
-    document.onclick = function () {
-        var h = window.innerWidth / loadingImg[0] * loadingImg[1];
-        $('#loading').css({opacity: 0})
-            .children('img')
-            .css({height: h, 'margin-bottom': getOffsetTop(h)});
-            //.css({height: h, top: getOffsetTop(h)});
-        setTimeout(function () {
-            $('#loading').remove();
-        }, 500);
-    };
-}());
+    zr.animate(image.id, '', true)
+        .when(1000, {
+            //opacity: 0,
+            position: [100, 100]
+        })
+        .start()
+});
