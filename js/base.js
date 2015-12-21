6b41d549aa378e4f5de25804cf22f3dd98e4d15b
Felix <1449704765@qq.com>
2015-12-21 22:15:05 +0800

    trial 5

--- js/base.js
+++ js/base.js
@@ -1,61 +1,70 @@
 /**
  * Created by Administrator on 2015/12/12.
  */
-//
+
 //var loadingImg = [500, 324];
 //window.getOffsetTop = function (h) {
 //    h = !h ? document.getElementById('loading').getElementsByTagName('img').item(0).clientHeight : h;
 //    var t = (window.innerHeight - h) / 2;
+//    alert(t)
 //    return t;
 //};
-//+(function () {
-//    $('#loading img').css({top: window.getOffsetTop()});
+//(function () {
+//    //$('#loading img').css({top: window.getOffsetTop(), opacity: 1}).addClass('loading-animate');
 //
-//    $('#menu').click(function () {
-//        alert('Clicked!');
-//    });
 //}());
-//
-//+(function () {
-//    document.onclick = function () {
-//        var h = window.innerWidth / loadingImg[0] * loadingImg[1];
-//        $('#loading').css({opacity: 0})
-//            .children('img')
-//            .css({height: h, 'top': getOffsetTop(h)});
-//        setTimeout(function () {
-//            $('#loading').remove();
-//        }, 500);
-//    };
-//}());
-
-require.config({
-    packages: [
-        {
-            name: 'zrender',
-            location: '../js/zrender',
-            main: 'zrender'
-        }
-    ]
+$.fn.touch = function (callback) {
+    this.get(0).addEventListener('touchstart', callback, false);
+}
+$('#menu').touch(function () {
+    alert('Clicked!');
 });
-
-require(['zrender', 'zrender/shape/Image'], function (zrender, ImageShape) {
-    var zr = zrender.init(document.body);
-    var image = new ImageShape({
-        position: [0, 0],
-        scale: [1, 1],
-        style: {
-            x: 0,
-            y: 0,
-            image: '../images/flying_elephant.jpg'
-        }
+//(function () {
+    //alert(3)
+    $(document).touch(function () {
+        $('#loading').css({'background-size': 500, 'opacity': 0});
     });
-    zr.addElement(image);
-    zr.render();
-
-    zr.animate(image.id, '', true)
-        .when(1000, {
-            //opacity: 0,
-            position: [100, 100]
-        })
-        .start()
-});
+    document.onclick = function () {
+        //var h = window.innerWidth / loadingImg[0] * loadingImg[1];
+        //$('#loading').css({'background-size': 500, 'opacity': 0});
+        //$('#loading').css({opacity: 0})
+        //    .children('img')
+            //.css({height: h, 'top': getOffsetTop(h)});
+        //setTimeout(function () {
+        //    $('#loading').remove();
+        //}, 500);
+    };
+//}());
+//
+//require.config({
+//    packages: [
+//        {
+//            name: 'zrender',
+//            location: '../js/zrender',
+//            main: 'zrender'
+//        }
+//    ]
+//});
+//
+//require(['zrender', 'zrender/shape/Image'], function (zrender, ImageShape) {
+//    return ;
+//    var zr = zrender.init(document.body);
+//    var image = new ImageShape({
+//        position: [0, 0],
+//        scale: [1, 1],
+//        style: {
+//            x: 0,
+//            y: 0,
+//            image: '../images/flying_elephant.jpg'
+//        }
+//    });
+//    zr.addElement(image);
+//    zr.render();
+//
+//    zr.animate(image.id, '', true)
+//        .when(1000, {
+//            //opacity: 0,
+//            position: [100, 100]
+//        })
+//        //.start()
+//});
