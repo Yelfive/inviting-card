3549a85e6c0b55383afc572b27cf32c8c2075ddb
Felix <1449704765@qq.com>
2015-12-31 16:45:20 +0800

    trial 12

--- js/base.js
+++ js/base.js
@@ -6,25 +6,14 @@
  *
  */
 +(function () {
-    const BG_INTERVAL = 50000;
+    function isHorizon() {
+        return window.orientation!== undefined && window.orientation !== 0;
+    }
+    if (isHorizon()) {
+        return alert('Please place your phone vertically.');
+    }
     var $body = document.body;
     var fk = {
-        previousCanvas: false,
-        images: [], // array of image dom(s)
-        previousImage: '', // index from fk.images
-        canvases: {},  // canvas dom(s)
-        setImage: function (img) {
-            this.images.push(img);
-        },
-        getImage: function () {
-            var index;
-            index = (Math.random() * (fk.images.length - 1)).toFixed();
-            if (index == fk.previousImage) {
-                index = index == 0 ? 1 : 0;
-            }
-            fk.previousImage = index;
-            return fk.images[index];
-        },
         addEvent: function (elem, event, callback) {
             if (elem.addEventListener) {
                 elem.addEventListener(event, callback, true);
@@ -32,89 +21,79 @@
                 alert('Failed to add event');
             }
         },
-        tick: {},
-        startDrawing: function () {
-            this.tick.drawing = setInterval(drawImage, BG_INTERVAL);
-        },
-        stopDrawing: function () {
-            var drawing;
-            if (drawing = this.tick.drawing) {
-                this.tick.drawing = clearInterval(drawing);
-            }
-        }
+        tick: {}
     };
-    var imageContainer = document.getElementById('container').getElementsByClassName('image')[0];
-    var canvasContainer = document.getElementById('container').getElementsByClassName('canvas')[0];
-    var images = ['flying_elephant', 'kitty_with_rose']; // image names
-    function downloadImage(draw) {
-        var img = new Image();
-        img.src = '../images/' + images.shift() + '.jpg';
-        img.onload = function () {
-            fk.setImage(img);
-            if (images.length) {
-                this.onload = null;
-                downloadImage();
-            }
-            if (draw) {
-                drawImage();
-            }
-            // TODO: remove dom, see if it increase efficiency
-        }
-    }
-
-    //downloadImage(true);
     $body.onload = function () {
         var $loading = document.getElementsByClassName('loading')[0];
         $loading.className = 'loading running';
         fk.addEvent($loading, 'animationend', function () {
             afterLoadingRemoved();
         });
-        /* Blur background */
-        fk.startDrawing();
     }
 
-    function drawImage(image) {
-        if (image instanceof Image) {
-            fk.stopDrawing();
-            fk.setImage(image);
-        } else {
-            image = fk.getImage();
-        }
-
-        if (image) {
-            var canvas;
-            if (canvas = fk.canvases[image.src]) { // Use the existed canvas
-                canvas.className = 'fadeIn';
-            } else { // Create new canvas
-                fk.canvases[image.src] = canvas = document.createElement('canvas');
-                canvas.className = 'fadeIn';
-                canvasContainer.appendChild(canvas);
-                stackBlurImage(image, canvas, 40);
-                fk.addEvent(canvas, 'animationend', function () {
-                    if (this.className == 'fadeOut' || fk.previousCanvas == canvas) {
-                        return false;
-                    }
-                    if (fk.previousCanvas) {
-                        fk.previousCanvas.className = 'fadeOut';
-                    }
-                    fk.previousCanvas = canvas;
-                });
-
-                // TODO: and class invisible, to see if it increase the speed(efficiency)
-                //fk.addEvent(canvas, 'animationstart', function () {
-                //    if (fk.previousImage < canvas) {
-                //
-                //    }
-                //});
-            }
-        }
-    }
     function afterLoadingRemoved() {
         document.body.removeChild(document.querySelector('.loading'));
     }
 
-    fk.addEvent(document.querySelector('#menu'), 'touchstart', function () {
-    });
+    var coordinate = {
+        config: {
+            radius: 0,
+            zero: {x:0, y: 0},
+            total: 0,
+            randomPosition: true
+        },
+        circle: function (count) {
+            if (this.config.total > 0 && this.config.radius > 0) {
+                var radian = 2 * Math.PI * count / this.config.total;
+                if (this.config.randomPosition === true) {
+                    radian += Math.random() * Math.PI / 18;
+                }
+                return {
+                    x: this.config.zero.x + this.config.radius * Math.cos(radian),
+                    y: this.config.zero.y + this.config.radius * Math.sin(radian)
+                }
+            } else {
+                throw new Error('Invalid config for coordinate.');
+            }
+        }
+    };
+    (function() {
+        var $circle = document.getElementById('circle-flowers');
+        var children = $circle.children;
+        var len = children.length;
+        var cw = $circle.clientWidth;
 
-    window.drawImage = drawImage;
+        radius = 0.4 * cw;
+        coordinate.config = {
+            radius: radius,
+            zero: {x: cw / 2, y: cw / 2},
+            total: len,
+            randomPosition: true
+        };
+        $circle.style.height = cw;
+        var i = 0;
+        //return ;
+        fk.tick.flowers = setInterval(function () {
+            var child = children[i++];
+            if (!child) {
+                clearInterval(fk.tick.flowers);
+                return fk.tick.flowers = null;
+            }
+            var c = coordinate.circle(i);
+            child.style.left = (c.x - child.clientWidth / 2) + 'px';
+            child.style.top = (c.y - child.clientHeight / 2) + 'px';
+            child.className += ' bloom';
+        }, 500);
+    }());
+
+    function typeIn(dom) {
+        if (!(dom instanceof HTMLElement)) {
+            return ;
+        }
+        var html = dom.innerHTML;
+
+        
+
+    }
+
 }());
