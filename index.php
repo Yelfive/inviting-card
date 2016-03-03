4cfd3c5975bc941c4e63ae44c391f63c479de898
Felix <1449704765@qq.com>
2016-03-03 09:49:15 +0800

    bac

--- index.php
+++ index.php
@@ -4,9 +4,7 @@
  * @author Felix Huang <yelfivehuang@gmail.com>
  */
 define('__APP__', __DIR__);
-define('DEBUG', true);
+//define('DEBUG', true);
 include __APP__ . '/library/fk.php';
-
-// todo theme, inviting
 
 \fk\web\Application::run();
